79de8c1fe0309bc3e2988e9c56b8f05deb4a8960
crisvaliente <valientecristian536@gmail.com>
2023-08-09 03:00:06 -0300

    Agregando productos
    Se crea el fetch para traer de la API los productos con sus características

---
+++ js/products.js
@@ -0,0 +1,29 @@
+function showData(dataArray) {
+    // El for...of itera sobre los elementos del arreglo
+    for (const item of dataArray) {
+      // En la siguiente línea se utilizan "backticks" para armar el String.
+      container.innerHTML += `
+        <p>ID: ${item.id}</p>
+        <p>Nombre: ${item.name}</p>
+        <p>Precio: ${item.cost} ${item.currency}</p>
+        <p>Descripción: ${item.description}</p>
+        <p>Vendidos: ${item.soldCount}</p>
+        <img src="${item.image}" alt="Imagen del producto">
+        <hr>
+      `;
+    }
+  }
+  
+
+
+
+  fetch(PRODUCTS_URL)
+    .then(response => response.json())
+    .then(data => {
+      const autosArray = data.products; // Asignamos el arreglo de productos del JSON a autosArray
+      showData(autosArray);
+    })
+    .catch(error => {
+      console.error("Error trayendo:", error);
+    });
+  
